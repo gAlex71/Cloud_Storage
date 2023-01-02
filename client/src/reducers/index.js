0a923eda0a09b2c801caeae6d93d1e2dac03a5b9
gAlex71 <alexvladimirovich71@mail.ru>
2023-01-02 11:19:24 +0300

    create store redux

---
+++ client/src/reducers/index.js
@@ -0,0 +1,14 @@
+import {applyMiddleware, combineReducers, createStore} from "redux"
+//Для использования инструментов разработчика в браузере
+import {composeWithDevTools} from "redux-devtools-extension"
+import thunk from "redux-thunk"
+import fileReducer from "./fileReducer"
+import userReducer from "./userReducer"
+
+//Корневой компонент, в котором будем совмещать все редюсеры
+const rootReducer = combineReducers({
+    user: userReducer,
+    files: fileReducer
+})
+
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
